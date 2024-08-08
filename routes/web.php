4971a0fbd8408ab66f18096a2c4e14ea2055c9f7
dbhasan <infoalihasanbd@gmail.com>
2024-08-08 09:53:11 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::post('/', [AuthController::class, 'adminlogin'])->name('admin.login');
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('profle-update',[AuthController::class,'profileupdate'])->name('profle.update');
+    Route::post('profle-update',[AuthController::class,'passwordupdate'])->name('password.update');
+});
