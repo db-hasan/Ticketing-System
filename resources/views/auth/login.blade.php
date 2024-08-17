6f6a942fb7ee98543b559738d6ee56fd7e42ebbc
dbhasan <infoalihasanbd@gmail.com>
2024-08-17 17:47:24 +0600

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
                                 <div class="card-body">
                                     <div class="pt-4 pb-2">
                                         <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                                        <p class="text-center small">Enter your username & password to login</p>
+                                        <p class="text-center small">Enter your Email & password to login</p>
                                     </div>
                                     <form method="POST" action="{{ route('admin.login') }}" class="row g-3">
                                         @csrf
